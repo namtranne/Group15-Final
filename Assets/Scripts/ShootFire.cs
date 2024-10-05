86c2c2de71d79e9aab5da8c8f79ee918176c5e69
Trần Hải Nam <steven.hai.ta856@gmail.com>
2024-10-06 01:32:49 +0700

    refactor: disable shooting dropship

--- Assets/Scripts/ShootFire.cs
+++ Assets/Scripts/ShootFire.cs
@@ -6,17 +6,44 @@ public class ShootFire : MonoBehaviour
 {
     public GameObject fireSkill; // Prefab of the fire skill
     public float fireInterval = 2f; // Time interval in seconds
+    private Coroutine shootCoroutine; // To hold the coroutine reference
 
     // Start is called before the first frame update
     void Start()
     {
-        // Repeatedly invoke the Shoot method every 2 seconds
-        InvokeRepeating("Shoot", fireInterval, fireInterval);
+        // Start the shooting coroutine
+        shootCoroutine = StartCoroutine(ShootRoutine());
+    }
+
+    // Coroutine to repeatedly shoot fire skills
+    private IEnumerator ShootRoutine()
+    {
+        while (true) // Infinite loop until stopped
+        {
+            Shoot(); // Call the Shoot method
+            yield return new WaitForSeconds(fireInterval); // Wait for the specified interval
+        }
     }
 
     // This method will spawn the fire skill at the firePoint's position
     void Shoot()
     {
         Instantiate(fireSkill, transform.position, transform.rotation);
+    }
+
+    // This method is called when the game object is disabled
+    void OnDisable()
+    {
+        // Stop the shooting coroutine when the dropship is disabled
+        if (shootCoroutine != null)
+        {
+            StopCoroutine(shootCoroutine);
+            shootCoroutine = null; // Clear the reference
+        }
+    }
+
+    void OnEnable()
+    {
+        shootCoroutine = StartCoroutine(ShootRoutine());
     }
 }
